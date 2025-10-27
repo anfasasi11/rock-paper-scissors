73e5aa1f566737d4905e4e6d2c504989a87892f2
anfas <anfasashiq@gmail.com>
2025-10-27 17:58:47 +0530

    Write clean Code

--- script.js
+++ script.js
@@ -1,67 +1,57 @@
-
-
 function getComputerchoice() {
-
-    let num = Math.floor(Math.random()*3) + 1 ;
-    
-    if(num == 1) {
-        
-        return "ROCK" ;
-    
-    } else if(num == 2) {
-        
-        return "PAPER" ;
-    
-    } else if(num == 3) {
-        
-        return "SCISSORS" ;
-    }
-}
-
-function getHumanchoice() {
-
-    let choice = prompt("Enter your Choice") ;
-    return choice.toUpperCase() ;
-    
-}
-
-let humnanScore = 0 ;
-let computerScore = 0 ;
-
-function playRound() {
-    
-    let humanSelection = getHumanchoice() ;
-    let computerSelection = getComputerchoice() ;
-
-    if(humanSelection != computerSelection) {
-
-        if(humanSelection == "ROCK" && computerSelection == "SCISSORS" || humanSelection == "SCISSORS" && computerSelection == "PAPER" || humanSelection == "PAPER" && computerSelection == "ROCK") {
-
-            humnanScore++ ;
-            console.log(`YOU WON!!/n${humanSelection} BEATS ${computerSelection}`) ;
-
-        } else {
+            let numRandom = Math.floor(Math.random()*3) + 1 ;
             
-            computerScore++ ;
-            console.log(`YOU LOSE!!/n${computerSelection} BEATS ${humanSelection}`) ;
-    
+            if(numRandom == 1) {               
+                return "ROCK";
+            
+            } else if(numRandom == 2) {                
+                return "PAPER";
+            
+            } else if(numRandom == 3) {                
+                return "SCISSORS";            
+            }
         }
 
-    } else console.log("ITS A DRAW!!") ;
+        function getHumanchoice() {
+            let choice = prompt("Enter your choice") ;
+            return choice.toUpperCase() ;
+            
+        }
 
-}
+        let humnanScore = 0 ;
+        let computerScore = 0 ;
 
-function playGame() {
-    
-    playRound() ;
-    playRound() ;
-    playRound() ;
-    playRound() ;
-    playRound() ;
+        function playRound() {         
+            let humanSelection = getHumanchoice() ;
+            let computerSelection = getComputerchoice() ;
 
-    console.log(`GAME OVER/nSCORE: ${humnanScore} || ${computerScore}`) ;
-}
+            if(humanSelection != computerSelection) {
 
-playGame() ;
+                if(humanSelection == "ROCK" && computerSelection == "SCISSORS" || 
+                   humanSelection == "SCISSORS" && computerSelection == "PAPER" || 
+                   humanSelection == "PAPER" && computerSelection == "ROCK") {
+                    humnanScore++ ;
+                    console.log(`YOU WON!!\n${humanSelection} BEATS ${computerSelection}`) ;
 
+                } else { 
+                    computerScore++ ;
+                    console.log(`YOU LOSE!!\n${computerSelection} BEATS ${humanSelection}`) ;
+            
+                }
 
+            } else console.log("ITS A DRAW!!") ;
+
+        }
+
+        function playGame() {
+            
+            playRound() ;
+            playRound() ;
+            playRound() ;
+            playRound() ;
+            playRound() ;
+
+            console.log(`GAME OVER\nSCORE: ${humnanScore} || ${computerScore}`) ;
+            humnanScore = 0 ;
+            computerScore = 0 ;
+        }
